b50a9b8d913ba68a2e33295e7817c181613276be
mgrose <mg@optum.com>
2021-01-28 17:04:57 -0600

    updates

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Build builder images
-DOCKER_BUILDKIT=1 docker build -f "build/package/alpine-builder/Dockerfile" -t "runiac:alpine-builder" . || exit 1
+
+rm -rf ./reports
+outputVolume=$(docker volume create)
+DOCKER_BUILDKIT=1 docker build -f "build/package/alpine-builder/Dockerfile" -t "runiac:alpine-builder" . || exit 1;
+CID=$(docker create -v "$outputVolume":/reports "runiac:alpine-builder")
+docker cp "$CID":/reports $(pwd)
+touch ./reports/*.xml
+docker rm "$CID"
+docker volume rm "$outputVolume"
+
 
 # Build consumer images
 for d in build/package/*/ ; do
